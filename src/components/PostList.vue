91948672dd722b601cee06937595276dc46943e7
MilTech124 <Jaroslawmatusiak124@gmail.com>
2021-07-22 15:23:38 +0200

    blog in Vue with json-serve

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    
+        <div v-for="post in posts" :key="post.id">
+    
+            <SinglePost :post="post" />
+    
+        </div>
+    
+    
+    
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+import SinglePost from './SinglePosts.vue'
+
+export default {
+    name: 'postList',
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component unmounted'))
+        onUpdated(() => console.log('component updated'))
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
